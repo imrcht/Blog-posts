a8b66128775da3cacdbb1f500c533afab96284a5
Rachit gupta <rachchooit@gmail.com>
2022-02-04 10:40:02 +0530

    some changes

--- security.js
+++ security.js
@@ -1,15 +1,15 @@
 exports.getSecurity = () => {
     confidential = {
-        user:"imrcht",
-        pwd: "rachoof7",
+        user:"sadfsafsd",
+        pwd: "asdfsafas",
     };
     return confidential;
 }
 
 exports.getSecret = () =>{
     auth = {
-        email: "rachitgupta.5036459@gmail.com",
-        password: "eouihfqlgftqflpd",
+        email: "adfasfdsads.adfasfasdc@gmail.com",
+        password: "dfsafsvadzvsvsad",
     }
     return auth;
 }
