84feb9231b65c32cadf7ce268c984298ea2a5d00
imrcht <rachchooit@gamil.com>
2022-07-01 00:10:07 +0530

    Updated version

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const { truncate } = require('lodash');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// const homeStartingContent =
-// 	'Hello guys, Welcome to my blog site. Here you can Post your thoughts and we will publish in this site. This is the home page , here you can see some points of different posts and when you click on read more it will redirect you to that post page. We have 5 options in the navigation bar. Through Compose you can create a new post. Compose option will only be visible when you are logged in.Through All posts you can see all the posts that has been posted in our blog.';
 const homeStartingContent =
-	'Special Home Content for today in this site. Specialy designed for RED STOLE. For today i.e. 24th June, this blog website is turned to 🎉BirthDay🎉 site. Bahot time laga meko ye changes karne me, bahut errors aa rahe the but still error solve karke ye changes kiye hai , party chahiye iske liye to 😂😅. More details/surprises in Particular post about Shreya 😇🤗';
+	'Hello guys, Welcome to my blog site. Here you can Post your thoughts and we will publish in this site. This is the home page , here you can see some points of different posts and when you click on read more it will redirect you to that post page. We have 5 options in the navigation bar. Through Compose you can create a new post. Compose option will only be visible when you are logged in.Through All posts you can see all the posts that has been posted in our blog.';
+// const homeStartingContent =
+// 	'Special Home Content for today in this site. Specialy designed for RED STOLE. For today i.e. 24th June, this blog website is turned to 🎉BirthDay🎉 site. Bahot time laga meko ye changes karne me, bahut errors aa rahe the but still error solve karke ye changes kiye hai , party chahiye iske liye to 😂😅. More details/surprises in Particular post about Shreya 😇🤗';
 const aboutContent =
 	'Hello, This website is one of the part of FIERCHT creations. fiercht is my insta handle. you can connect me on there also. Also some more project by FIERCHT creations are listed below: ';
 const contactContent =
